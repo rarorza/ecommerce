46ee18bb1fd9ea3f9c81d52824572f7df099ac3b
Rafael R Rosa <rarorza@proton.me>
2024-04-22 15:34:58 -0300

    feat: implements user and profile serializer

---
+++ backend/userauth/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from userauth.models import Profile, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
