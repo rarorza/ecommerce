932fa8189613262811c1d9c86db524b67e714345
Rafael R Rosa <rarorza@proton.me>
2024-04-22 17:10:00 -0300

    feat: implements a custom jwt serializer to login

--- backend/userauth/serializers.py
+++ backend/userauth/serializers.py
@@ -1,5 +1,21 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token
 from userauth.models import Profile, User
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user: User) -> Token:
+        token = super().get_token(user)
+        token["full_name"] = user.full_name
+        token["email"] = user.email
+        token["username"] = user.username
+        try:
+            token["vendor_id"] = user.vendor_id
+        except ValueError:
+            token["vendor_id"] = 0
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
