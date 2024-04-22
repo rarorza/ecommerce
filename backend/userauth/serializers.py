c79e4e7d855c5e2b8362e7c67d1bc6fb315aae35
Rafael R Rosa <rarorza@proton.me>
2024-04-22 17:51:37 -0300

    feat: implements register serializer

--- backend/userauth/serializers.py
+++ backend/userauth/serializers.py
@@ -1,7 +1,40 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import Token
 from userauth.models import Profile, User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validadors=[validate_password]
+    )
+    password2 = serializers.CharField(
+        write_only=True, required=True, validadors=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = ["full_name", "email", "phone", "password", "password2"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password does not match"}
+            )  # noqa 501
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data["full_name"],
+            email=validated_data["email"],
+            phone=validated_data["phone"],
+        )
+        username, _ = user.email.split("@")
+        user.username = username
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
