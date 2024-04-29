013daeba5dd3a3a61c0197da45b9ff4476d87d43
Rafael R Rosa <rarorza@proton.me>
2024-04-29 19:15:02 -0300

    feat and setup: implements yasg swagger to show API doc and changes dependencies versions

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,10 +19,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce Backend APIs",
+        default_version="v1",
+        description="This is the documentation for the backend API",
+        terms_of_service="https://mywebsite.com/policies/",
+        contact=openapi.Contact(email="rarorza@proton.me"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
