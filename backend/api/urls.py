58abec44f0fbbc4524572fbd80066aec1c149fa6
Rafael R Rosa <rarorza@proton.me>
2024-04-29 16:52:10 -0300

    feat: implements register api view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from userauth import views as userauth_views
 
 urlpatterns = [
     path("user/token/", userauth_views.MyTokenObtainPairView.as_view()),
+    path("user/register/", userauth_views.RegisterView.as_view()),
 ]
--- backend/userauth/views.py
+++ backend/userauth/views.py
@@ -1,3 +1,5 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from userauth.models import Profile, User
 from userauth.serializers import MyTokenObtainPairSerializer, RegisterSerializer
@@ -5,3 +7,9 @@ from userauth.serializers import MyTokenObtainPairSerializer, RegisterSerializer
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
