77d2c5aaa9a7a4d076c091c6c1042507a5831807
Rafael R Rosa <rarorza@proton.me>
2024-05-08 11:15:24 -0300

    feat: extends axios lifecycle

---
+++ frontend/src/utils/useAxios.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+import { isAccessTokenExpired, setAuthUser, getRefreshToken } from "./auth"
+import { BASE_URL } from "./constants"
+import Cookies from 'js-cookie'
+
+// Will intercepts every request to the server and validated the refresh token
+const useAxios = async () => {
+  const access_token = Cookies.get('access_token')
+  const refresh_token = Cookies.get('refresh_token')
+
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {Authorization: `Bearer ${access_token}`}
+  })
+
+  axiosInstance.interceptors.request.use(async (req) => {
+    if (!isAccessTokenExpired(access_token)) {
+      return req
+    }
+    const responseToken = await getRefreshToken(refresh_token)
+    setAuthUser(responseToken.access, responseToken.refresh)
+    
+    req.headers.Authorization = `Bearer ${responseToken.data.access}`
+    return req
+  })
+
+  return axiosInstance
+}
+
+export default useAxios
