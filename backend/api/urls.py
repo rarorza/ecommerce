d4b898828d1933fac1ab8c295c06772ebb1bc89b
Rafael R Rosa <rarorza@proton.me>
2024-05-07 21:03:23 -0300

    feat: implements authentification functions in react and a new endpoint in django to check refresh token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from store import views as store_views
 from userauth import views as userauth_views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("user/token/", userauth_views.MyTokenObtainPairView.as_view()),
+    path("user/token/refresh/", TokenRefreshView.as_view()),
     path("user/register/", userauth_views.RegisterView.as_view()),
 ]
